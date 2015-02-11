5fdd0668e661fb506e81a13635ddd142a9ae48f9
unknown <300229836@QQ0005CG4410T64.code1.emi.philips.com>
2015-02-11 10:34:35 +0100

    Field parsing fixed!
    Changes to be committed:
    	modified:   hybrid_topology.m

--- hybrid_topology.m
+++ hybrid_topology.m
@@ -15,43 +15,48 @@ function [ topology ] = hybrid_topology(Arch,duty,opt)
 %   May be freely used and modified but never sold.  The original author
 %   must be cited in all derivative work.
 
-dc_out = opt.dc_out;
+if isfield('dc_out',opt)
+    dc_out = opt.dc_out;
+else
+    dc_out = 1;
+end
+
 
 if (nargin == 1) || isempty(duty) 
     duty = 0.5;
 end
 
 %% Create class 
-gent_top =  generic_switched_capacitor_class(Arch,'Duty',duty);
+gen_top =  generic_switched_capacitor_class(Arch,'Duty',duty);
 
 %The functions only return the ouput nodes used in hybrid cell with the
 %excursion of the all dc-outputs
-OutNodes = 1:gent_top.n_outs;
+OutNodes = 1:gen_top.n_outs;
 
 if ~dc_out
     if n_caps > 2
-        OutNodes([gent_top.dc_out_cap end])=[];
+        OutNodes([gen_top.dc_out_cap end])=[];
     else
-        OutNodes([gent_top.dc_out_cap])=[];
+        OutNodes([gen_top.dc_out_cap])=[];
     end
 end
 
 
 %Generate output structures
-topology.ratio = gent_top.m_ratios(OutNodes);
-topology.vc = gent_top.v_caps_norm.'; %Capacitor voltages voltages
-topology.vr = gent_top.v_sw_norm; %Switches voltages
-topology.Y_ssl = gent_top.k_ssl;
-topology.Y_fsl = gent_top.k_fsl;
+topology.ratio = gen_top.m_ratios(OutNodes);
+topology.vc = gen_top.v_caps_norm.'; %Capacitor voltages voltages
+topology.vr = gen_top.v_sw_norm; %Switches voltages
+topology.Y_ssl = gen_top.k_ssl;
+topology.Y_fsl = gen_top.k_fsl;
 
 topology.f_ssl = ... %Retrun the symbolic impedance function normalized respect 1Hz 
-    gent_top.r_ssl(OutNodes); 
+    gen_top.r_ssl(OutNodes); 
 
 topology.f_fsl = ... %Retrun the symbolic fsl impedance function of the switches 
-    subs(gent_top.r_fsl(OutNodes),gent_top.esr_caps,zeros(1,gent_top.n_caps));
+    subs(gen_top.r_fsl(OutNodes),gen_top.esr_caps,zeros(1,gen_top.n_caps));
 
 topology.f_esr = ... %Retrun the symbolic fsl impedance function of the esr capacitors 
-    subs(gent_top.r_fsl(OutNodes),gent_top.ron_switches,zeros(1,gent_top.n_switches));
+    subs(gen_top.r_fsl(OutNodes),gen_top.ron_switches,zeros(1,gen_top.n_switches));
 
 topology.var_ssl = symvar(topology.f_ssl);
 
@@ -66,23 +71,23 @@ topology.var_fesr = symvar(topology.f_esr);
 topology.eval_fesr = @(x)... %Returns a function that evaluates the Output Impedance as function
      subs(topology.f_esr,topology.var_fesr,x); %of flying capacitances 
      
-topology.vo_swing = 1/n_caps;
+topology.vo_swing = 1/gen_top.n_caps;
 topology.duty = duty;    
-topology.g = gent_top.k_factors;
+topology.g = gen_top.k_factors;
 
-topology.dc_outputs = gent_top.dc_out_cap;
-topology.r = cat(3,gent_top.phase{1}.r_vector,gent_top.phase{2}.r_vector); 
+topology.dc_outputs = gen_top.dc_out_cap;
+topology.r = cat(3,gen_top.phase{1}.r_vector,gen_top.phase{2}.r_vector); 
 topology.r_vars = symvar(topology.r);
 topology.eval_r =@(x) subs(topology.r,topology.r_vars,x);
-topology.q_dc = [gent_top.phase{1}.r_vector(end,gent_top.dc_out_cap) gent_top.phase{2}.r_vector(end,gent_top.dc_out_cap)];
+topology.q_dc = [gen_top.phase{1}.r_vector(end,gen_top.dc_out_cap) gen_top.phase{2}.r_vector(end,gen_top.dc_out_cap)];
 topology.eval_q_dc = @(x)... %Returns a function that evaluates the Output Impedance as function
      subs(topology.q_dc,symvar(topology.q_dc),x); %of flying capacitances 
 
 topology.N_outs = length(topology.ratio);
-topology.N_sw     = gent_top.n_switches;
-topology.N_caps   = gent_top.n_caps;
+topology.N_sw     = gen_top.n_switches;
+topology.N_caps   = gen_top.n_caps;
 
-topology.ph = gent_top.phase;
+topology.ph = gen_top.phase;
 end
 
 
